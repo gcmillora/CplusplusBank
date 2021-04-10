b895b6b6764f3a8142667f7c2fdec87f99120a38
Greg Norman Millora <59990974+gcmillora@users.noreply.github.com>
2021-04-10 15:03:29 +0800

    New solutions.

---
+++ 594-15.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    double heights[] = {5.2,6.3,5.8,4.9,5.2,5.7,6.7,7,7.1,5.10,6.0};
+    int weights[] = {120,125,137,140,150,180,210};
+    char specialSymbols[] = {'$','#','%','@','&','!','^'};
+    string seasons[] = {"fall","winter","spring","summer"};
+}
